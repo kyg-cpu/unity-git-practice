733ccb9a021e0813418f6a088caa237d92b1a846
kyg-cpu <gini7638@gmail.com>
2026-09-16 16:45:02 +0900

    큐브 추가 및 파티클 생성

--- Unity/Assets/new.cs
+++ Unity/Assets/new.cs
@@ -5,7 +5,7 @@ public class new : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Test
     }
 
     // Update is called once per frame
